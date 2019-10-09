9b308a5c59d87fe1d087695ce787f7fc0d92e05c
qinliansong <qinliansong@baidu.com>
2019-10-09 16:42:37 +0800

    "five"

--- functional_tests.py
+++ functional_tests.py
@@ -29,23 +29,17 @@ class NewVisitorTest(unittest.TestCase):
         # 她在一个文本框中输入了“Buy peacock feathers(”购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
-
+        inputbox.send_keys(Keys.ENTER)
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
-        inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(rows.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
         # 页面中又显示了一个文本框，可以输入其他的待办事项
-        # 她输入了“Use peacock feathers to make a fly(”使用孔雀羽毛做假蝇)
+        # 她输入了使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
         self.fail('finish the test!')
-
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
