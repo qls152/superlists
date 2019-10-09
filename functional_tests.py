90583a5ce2d409098d326548b0efc16f31da9ffc
qinliansong <qinliansong@baidu.com>
2019-10-09 10:37:08 +0800

    "使用注释编写规格的首个功能测试， 而且使用了 unittest"

---
+++ functional_tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
