4018d42f500dbb574d3324be9298db05efc69295
qinliansong <qinliansong@baidu.com>
2019-10-10 11:43:31 +0800

    "genggai"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import unittest
 # 解决相应问题而添加的包
 from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -50,8 +50,12 @@ class NewVisitorTest(LiveServerTestCase):
         # 她在一个文本框中输入了“Buy peacock feathers(”购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
+
+        # 她按回车键后，被带到了一个新URL
+        # 这个页面的待办事项清单中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        # 她按回车键后，页面更新了
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         # 待办事项表格中显示了“1: Buy peacock feathers”
         with self.wait_for_page_load(timeout=10):
             self.check_for_row_in_list_table("1: Buy peacock feathers")
@@ -66,7 +70,43 @@ class NewVisitorTest(LiveServerTestCase):
             self.check_for_row_in_list_table('1: Buy peacock feathers')
             self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('finish the test!')
+        # 现在一个叫作弗朗西斯的新用户访问了网站
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.maximize_window()
+        self.browser.implicitly_wait(8)
+
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('But milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，去睡觉了
+
+
+
+
+
 
 
 
