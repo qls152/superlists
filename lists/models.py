c60d60210d46c90a69cd3a56bcfe2290b262268a
qinliansong <qinliansong@baidu.com>
2019-10-10 15:34:37 +0800

    "genggai"

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+class List(models.Model):
+    pass
 
-# Create your models here.
+
 class Item(models.Model):
     text = models.TextField()
+    list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
+
