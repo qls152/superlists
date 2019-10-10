4154c194d57bb00b06ed4fb87fc849acd01c2049
qinliansong <qinliansong@baidu.com>
2019-10-10 19:25:03 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -7,13 +8,14 @@ from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(30)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     @contextmanager
@@ -111,15 +113,19 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 她看到输入框完美地居中显示
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing\n')
-        self.assertAlmostEqual(
+        self.assertNotAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=5
         )
+        inputbox.send_keys('testing\n')
 
-
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            272,
+            delta=5
+        )
 
 
 
